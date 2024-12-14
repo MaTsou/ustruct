f56b97e8d797a6dd975f7e670f6f1753c5d0b8ed
MaTsou <contact@logicore.fr>
2024-12-14 11:19:46 +0100

    Ustruct class is no more embedded in a Ustruct module.

--- lib/ustruct.rb
+++ lib/ustruct.rb
@@ -2,36 +2,30 @@
 
 require_relative "ustruct/version"
 
-module Ustruct
-
-  class Ustruct
-    def initialize( content = {} )
-      @content = content
-    end
-
-    def debug( keys = false )
-      keys ? @content.keys : @content
-    end
-
-    def to_hash
-      @content
-    end
-
-    def method_missing( name, *args )
-      args.empty? ? get( name ) : super
-    end
-
-    def []( key )
-      get( key )
-    end
-
-    private
-
-    def get( key )
-      @content.fetch( key.to_sym, @content.fetch( key.to_s, nil ) )
-    end
+class Ustruct
+  def initialize( content = {}, **options )
+    @content = content.merge( options )
   end
 
-  class Error < StandardError; end
-  # Your code goes here...
+  def debug( keys = false )
+    keys ? @content.keys : @content
+  end
+
+  def to_hash
+    @content
+  end
+
+  def method_missing( name, *args )
+    args.empty? ? get( name ) : super
+  end
+
+  def []( key )
+    get( key )
+  end
+
+  private
+
+  def get( key )
+    @content.fetch( key.to_sym, @content.fetch( key.to_s, nil ) )
+  end
 end
